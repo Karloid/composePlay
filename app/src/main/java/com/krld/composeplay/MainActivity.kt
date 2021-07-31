1f26a068f6ba2fa311cc8836b91d64e338df6b07
FIRST_NAME LAST_NAME <you@example.com>
2021-08-01 01:52:57 +0300

    - lazy column

--- app/src/main/java/com/krld/composeplay/MainActivity.kt
+++ app/src/main/java/com/krld/composeplay/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -71,8 +73,10 @@ class MainActivity : ComponentActivity() {
         Surface(color = MaterialTheme.colors.background) {
             Column {
                 Text(text = "Logged as ${state.currentUser.username}")
-                state.chats.forEach {
-                    ChatBubble(state, it)
+                LazyColumn() {
+                    items(state.chats) {
+                        ChatBubble(state, it)
+                    }
                 }
             }
         }
